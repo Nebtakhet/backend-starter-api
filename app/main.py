c6922bcd4ba95e7d1531471dc7d1152d80810608
César <cesasanc@student.hive.fi>
2026-02-03 00:36:28 +0200

    Use lifespan startup handler

--- app/main.py
+++ app/main.py
@@ -1,3 +1,5 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
 from app.api.v1.api import api_router
@@ -7,13 +9,15 @@ from app.db.session import engine
 
 configure_logging()
 
-app = FastAPI(title="Backend Starter API")
-app.include_router(api_router, prefix="/api/v1")
 
-
-@app.on_event("startup")
-def on_startup() -> None:
+@asynccontextmanager
+async def lifespan(app: FastAPI):
     Base.metadata.create_all(bind=engine)
+    yield
+
+
+app = FastAPI(title="Backend Starter API", lifespan=lifespan)
+app.include_router(api_router, prefix="/api/v1")
 
 
 @app.get("/health")
