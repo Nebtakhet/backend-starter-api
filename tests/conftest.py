c0476434785216047c447a78f5e9bf83b2e6bc24
César <cesasanc@student.hive.fi>
2026-02-03 00:17:23 +0200

    Added conftest

---
+++ tests/conftest.py
@@ -0,0 +1,11 @@
+import pytest
+
+from app.db.base import Base
+from app.db.session import engine
+
+
+@pytest.fixture(scope="session", autouse=True)
+def setup_database():
+    Base.metadata.create_all(bind=engine)
+    yield
+    Base.metadata.drop_all(bind=engine)
